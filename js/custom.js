2c74784d600fc05706ff3c69e88c84e5a3d00a0b
Amin Shazrin <aminshazrin@yahoo.com>
2017-04-02 17:48:57 +0800

    Dah sampai pricing section dah. Powerrrrrrrrrrrrr :D

--- js/custom.js
+++ js/custom.js
@@ -36,3 +36,18 @@ $(function () {
 
     });
 });
+/*=================================================================
+                    TESTIMONIALS
+
+==================================================================*/
+$(function () {
+    $("#customers-testimonials").owlCarousel({
+
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+
+    });
+});
